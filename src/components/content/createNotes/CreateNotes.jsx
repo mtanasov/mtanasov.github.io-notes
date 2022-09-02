303f7dde5a42d5de14ffcfe9094cabd724296c26
Mykhailo Tanasov <mtanasov@gmail.com>
2022-09-02 12:18:15 +0300

    поправка цветотегов

--- src/components/content/createNotes/CreateNotes.jsx
+++ src/components/content/createNotes/CreateNotes.jsx
@@ -23,7 +23,7 @@ export function CreateNotes() {
    let iDate = React.createRef();
    let iTime = React.createRef();
    let iEPlace = React.createRef();
-   let iBookMark = "hsl(58deg 73% 78%)";
+   let iBookMark = "var(--pColorYellow)";
 
    const bgrClr = (event) => {
       iBookMark = event.target.style.backgroundColor
@@ -37,7 +37,7 @@ export function CreateNotes() {
       iDate.current.value = ""
       iTime.current.value = ""
       iEPlace.current.value = ""
-      iBookMark = "hsl(58deg 73% 78%)"
+      iBookMark = "var(--pColorYellow)"
    }
 
    return (
